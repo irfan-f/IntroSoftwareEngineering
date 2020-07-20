efb189e3d861f552af842745c3d8d31bb3175cdc
irfan-f <irfan.filipovic23@gmail.com>
2020-07-20 14:37:24 -0700

    project 3 added

---
+++ proj3-jsa/vocab/flask_vocab.py
@@ -0,0 +1,141 @@
+"""
+Flask web site with vocabulary matching game
+(identify vocabulary words that can be made 
+from a scrambled string)
+"""
+
+import flask
+import logging
+
+# Our own modules
+from letterbag import LetterBag
+from vocab import Vocab
+from jumble import jumbled
+import config
+
+###
+# Globals
+###
+app = flask.Flask(__name__)
+
+CONFIG = config.configuration()
+app.secret_key = CONFIG.SECRET_KEY  # Should allow using session variables
+
+#
+# One shared 'Vocab' object, read-only after initialization,
+# shared by all threads and instances.  Otherwise we would have to
+# store it in the browser and transmit it on each request/response cycle,
+# or else read it from the file on each request/responce cycle,
+# neither of which would be suitable for responding keystroke by keystroke.
+
+WORDS = Vocab(CONFIG.VOCAB)
+
+###
+# Pages
+###
+
+
+@app.route("/")
+@app.route("/index")
+def index():
+    """The main page of the application"""
+    flask.g.vocab = WORDS.as_list()
+    flask.session["target_count"] = min(
+        len(flask.g.vocab), CONFIG.SUCCESS_AT_COUNT)
+    flask.session["jumble"] = jumbled(
+        flask.g.vocab, flask.session["target_count"])
+    flask.session["matches"] = []
+    app.logger.debug("Session variables have been set")
+    assert flask.session["matches"] == []
+    assert flask.session["target_count"] > 0
+    app.logger.debug("At least one seems to be set correctly")
+    return flask.render_template('vocab.html')
+
+
+@app.route("/keep_going")
+def keep_going():
+    """
+    After initial use of index, we keep the same scrambled
+    word and try to get more matches
+    """
+    flask.g.vocab = WORDS.as_list()
+    return flask.render_template('vocab.html')
+
+
+@app.route("/success")
+def success():
+    return flask.render_template('success.html')
+
+###############
+# AJAX request handlers
+#   These return JSON, rather than rendering pages.
+###############
+
+
+@app.route("/_countem")
+def doit():
+    """
+    Get the text from #attempt after a keyup (from vocab.html)
+    Check various parameters using provided functions
+    Return these parameters in JSON object
+    Is listened for in vocab.html
+    """
+    app.logger.debug("Got a JSON request, Checking text")
+    text = flask.request.args.get("text", type=str)
+    jumble = flask.session["jumble"]
+    j_len = len(jumble)
+    in_jumble = LetterBag(jumble).contains(text)
+    matches = flask.session.get("matches", [])
+    matched = WORDS.has(text)
+    target = flask.session["target_count"]
+    
+    rslt = {"is_match": matched, "word": text, "matches": matches, 
+            "in_jumble": in_jumble, "len_j": j_len, "target": target}
+    return flask.jsonify(result=rslt)
+
+
+#################
+# Functions used within the templates
+#################
+
+@app.template_filter('filt')
+def format_filt(something):
+    """
+    Example of a filter that can be used within
+    the Jinja2 code
+    """
+    return "Not what you asked for"
+
+###################
+#   Error handlers
+###################
+
+
+@app.errorhandler(404)
+def error_404(e):
+    app.logger.warning("++ 404 error: {}".format(e))
+    return flask.render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def error_500(e):
+    app.logger.warning("++ 500 error: {}".format(e))
+    assert not True  # I want to invoke the debugger
+    return flask.render_template('500.html'), 500
+
+
+@app.errorhandler(403)
+def error_403(e):
+    app.logger.warning("++ 403 error: {}".format(e))
+    return flask.render_template('403.html'), 403
+
+
+####
+
+if __name__ == "__main__":
+    if CONFIG.DEBUG:
+        app.debug = True
+        app.logger.setLevel(logging.DEBUG)
+        app.logger.info(
+            "Opening for global access on port {}".format(CONFIG.PORT))
+        app.run(port=CONFIG.PORT, host="0.0.0.0")
